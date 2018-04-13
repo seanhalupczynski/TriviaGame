7ad212f6144a946648b9e1175d35f3107696436e
Sean Halupczynski <sean.halup@gmail.com>
2018-04-13 16:39:22 -0400

    All is working but need to loop through questions and display final results

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,96 +4,123 @@ $("#text").append("<h3>The game will start when you click the Start button</h3>"
 $("#text").append("<button class='btn btn-default' id='start'>Start</button>");
 
 var questions = {firstQuestion: {
-                        question: "What is the first question?",
-                        answers: ["This is the first answer.", "This is the second answer.", "This is the third answer.", "This is the fourth answer."],
-                        correctAnswer: function() {return this.answers[0];}
+                        question: "From Nelly's 2001 release '#1', you are?",
+                        answers: ["I am #1", "I am #2, who is not a winner", "I am #3, who nobody remembers", "None of the above"],
+                        correctAnswer: "I am #1"
                     },
                     secondQuestion: {
-                        question: "What is the second question?",
-                        answers: ["This is the first answer.", "This is the second answer.", "This is the third answer.", "This is the fourth answer."],
-                        correctAnswer: function() {return this.answers[0];}
+                        question: "To quote Muhammad Ali, you are?",
+                        answers: ["Average", "The worst", "THE GREATEST", "Just okay"],
+                        correctAnswer: "THE GREATEST"
                     },                    
                     thirdQuestion: {
-                        question: "What is the third question?",
-                        answers: ["This is the first answer.", "This is the second answer.", "This is the third answer.", "This is the fourth answer."],
-                        correctAnswer: function() {return this.answers[0];}
+                        question: "Under Armor wants you to protect what?",
+                        answers: ["This sofa", "This house", "This bed", "This cell phone"],
+                        correctAnswer: "This house"
                     },
                     fourthQuestion: {
-                        question: "What is the fourth question?",
-                        answers: ["This is the first answer.", "This is the second answer.", "This is the third answer.", "This is the fourth answer."],
-                        correctAnswer: function() {return this.answers[0];}
+                        question: "According to the band Survivor, you have the eye of the what?",
+                        answers: ["The potato", "", "The goldfish", "The mole", "The tiger"],
+                        correctAnswer: "The tiger"
                     },
                     fifthQuestion: {
-                        question: "What is the fifth question?",
-                        answers: ["This is the first answer.", "This is the second answer.", "This is the third answer.", "This is the fourth answer."],
-                        correctAnswer: function() {return this.answers[0];}
+                        question: "What time is it?",
+                        answers: ["Tooth hurty", "Nap time", "Game time! Wooo!", "Lunch time"],
+                        correctAnswer: "Game time! Woo!"
                     }
                 };
+                console.log(questions.firstQuestion.correctAnswer)
 var seconds;
-var timer = 10;
-function timeUp() {
+var questionTimer = 10;
+// var timer2 = 5;
+var answerCounter = 0;
+function stopTimer() {
     clearInterval(seconds);
 }
 // Start game
 $("#start").click(function() {
 
-    // Setting the timer to decrement by 1 second
+    // Function that sets the timer to decrement by 1 second
     function countdown(){
         clearInterval(seconds);
         seconds = setInterval(decrement, 1000);
     };
 
-    // Displaying the timer
-    // Starting at 20 seconds
-    $("#header").html("<h1>Trivia Challenge</h1><h2>Time Left: " + timer + "</h2>");
+    // Display the timer
+    $("#header").html("<h1>Faking Confidence Trivia Challenge</h1><h2>Time Left: " + questionTimer + "</h2>");
 
     // Starts decrementing the timer
     function decrement(){
-        timer--;
+        questionTimer--;
         // Displays the time left to answer
-        $("#header").html("<h1>Trivia Challenge</h1><h2>Time Left: " + timer + "</h2>");
+        $("#header").html("<h1>Faking Confidence Trivia Challenge</h1><h2>Time Left: " + questionTimer + "</h2>");
 
-        // Time up at 0 seconds
-        if(timer === 0) {
-            timeUp();
+        // What happens when time expires
+        if(questionTimer === 0) {
+            stopTimer();
             alert("Time is up!");
             $("#text").html("<h3>Oh no! You are out of time.</h3>")
+            $("#text").append("<h3>The correct answer : " + questions.firstQuestion.correctAnswer + "</h3>");
+            $("#text").append("<img src='assets/images/surprised.jpg'>");
+            $("#answers").hide();
         };
     };
 
     // Display first question
     $("#text").html("<h3>" + questions.firstQuestion.question + "</h3>");
 
-    // Display the answers in their own buttons
+    // Function to display the answers in their own buttons
     $("#answers").html(function (){
         for(i=0; i<questions.firstQuestion.answers.length; i++){
             $("#answers").append("<button class='btn btn-default' id='answerButton'>"+questions.firstQuestion.answers[i]+"</button>");
+            // console.log(questions.firstQuestion.answers[i]);
+            // console.log(questions.firstQuestion.correctAnswer);
         };
     });
 
-    // var userAnswer = $("#answers").click(function() {return questions.firstQuestion.answers.indexOf(click);});
-    // var userAnswer = function (event) {
-    //     for(b=0; b<questions.firstQuestion.answers.length; b++) {
-    //         button.addEventListener("click", function(){
-    //             return questions.firstQuestion.answers[b];
-    //         })
-    //     };
-    // };
-    // console.log(userAnswer);
+    // On click function to check if answer is right or wrong
+    $("#answers").click(function(event){
+        console.log(document.activeElement.textContent);
+        // If the user chooses the correct answer
+        if(document.activeElement.textContent === questions.firstQuestion.correctAnswer) {
+            // Add 1 to counter for correct answer
+            answerCounter++;
+            console.log(answerCounter);
+            // Stop the timer
+            stopTimer();
+            // Tell user they answered correctly
+            $("#text").html("<h3>You are correct!!!</h3>");
+            // Display the user's answer
+            $("#text").append("<h3>Your answer: " + document.activeElement.textContent + "</h3>");
+            // Display the correct answer
+            $("#text").append("<h3>The correct answer : " + questions.firstQuestion.correctAnswer + "</h3>");
+            // Display thumbs up image
+            $("#text").append("<img src='assets/images/thumbsup.jpg'>");
+            // Hide the answer buttons to prevent multiple answers
+            $("#answers").hide();
+        }
+        // If the user chooses a wrong answer
+        else {
+            console.log(answerCounter);
+            // Stop the timer
+            stopTimer();
+            // The the user they answered incorrectly
+            $("#text").html("<h3>You are wrong.</h3>");
+            // Display the user's answer
+            $("#text").append("<h3>Your answer: " + document.activeElement.textContent + "</h3>");
+            // Display the correct answer
+            $("#text").append("<h3>The correct answer : " + questions.firstQuestion.correctAnswer + "</h3>");
+            // Display sad image
+            $("#text").append("<img src='assets/images/sad.jpg'>");
+            // Hide the answer buttons to prevent multiple answers
+            $("#answers").hide();
+        } 
+        
+        
+    });
+
     countdown()
 
-    // Answering/Time is up
-    // if ($("#answers").click(function(){return questions.firstQuestion.answers.indexOf(answers)} === questions.firstQuestion.correctAnswer)) {
-    //     $("#text").html("<h3>That IS the correct answer!!!</h3>")
-    // }
-    // else if ($("#answers").click(function(){return questions.firstQuestion.answers.indexOf(answers)} !== questions.firstQuestion.correctAnswer)) {
-    //     $("#text").html("<h3>That is NOT the correct answer.</h3>")
-
-    // }
-    // if (timer === 0) {
-    //     $("#text").html("<h3>Oh no! You ran out of time.</h3>");
- 
-    // };
 });                
                 
 });
